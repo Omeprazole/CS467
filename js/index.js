04013d2e2faf5c2f4a18bf4021550c0b70e73613
Default <Default>
2017-10-12 19:40:05 -0400

    js functions for iframe
    js functions for iframe

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function load_cardboard(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/cardboard.html"
+};
+
+function load_paper(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/paper.html"
+};
+
+function load_cans(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/cans.html"
+};
+
+function load_plastic(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/plastic.html"
+};
+
+function load_glass(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/glass.html"
+};
+
+function load_compost(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/glass.html"
+};
+
+function load_landfill(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/glass.html"
+};
